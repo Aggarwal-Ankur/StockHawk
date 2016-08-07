4e7d15a52433ba6c23489fb3b00d1368b435791f
Ankur Aggarwal <roc.windwarrior@gmail.com>
2016-08-07 20:37:01 +0530

    Begin coding widget

---
+++ app/src/main/java/com/sam_chordas/android/stockhawk/widget/StockWidgetProvider.java
@@ -0,0 +1,36 @@
+package com.sam_chordas.android.stockhawk.widget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.sam_chordas.android.stockhawk.R;
+import com.sam_chordas.android.stockhawk.ui.MyStocksActivity;
+
+/**
+ * Created by Ankur on 07-Aug-16.
+ */
+
+public class StockWidgetProvider extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+        // Get all ids
+        ComponentName currentWidget = new ComponentName(context, StockWidgetProvider.class);
+
+        int[] allWidgetIds = appWidgetManager.getAppWidgetIds(currentWidget);
+
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.single_stock_widget_layout);
+
+        // Create an Intent to launch MainActivity
+        Intent intent = new Intent(context, MyStocksActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        views.setOnClickPendingIntent(R.id.widget, pendingIntent);
+
+    }
+}
---
+++ app/src/main/java/com/sam_chordas/android/stockhawk/widget/WidgetConfigActivity.java
@@ -0,0 +1,15 @@
+package com.sam_chordas.android.stockhawk.widget;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sam_chordas.android.stockhawk.R;
+
+public class WidgetConfigActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_widget_config);
+    }
+}
